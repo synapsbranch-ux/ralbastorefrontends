f8fd656f5fb6ce816ead953ab21c96bedce61a13
Samuel Jonathan JOSEPH <isteah.josephsamuel@gmail.com>
2025-10-27 16:41:29 -0400

    hello test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   instagram_token: 'INSTA_TOKEN',
   stripe_token: 'STRIPE_TOKEN',
   paypal_token: 'AUqwOlb75yY_oKcU0hud9J8Rjc0_dVJJn5qHSeYZJ-eIBk-iQl5dI7o_isPd5l68aP1WjZkZKdvay1_a',
-  baseUrl: 'http://13.217.198.39:5000/api/v1/',
+  baseUrl: 'https://13.217.198.39:5000/api/v1/',
   storeUrl: 'https://store.ralbatech.com'
 };
