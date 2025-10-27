973eafecdf29aa62823efc22cb2ab752a45262de
Samuel Jonathan JOSEPH <isteah.josephsamuel@gmail.com>
2025-10-27 16:54:46 -0400

    hello test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   stripe_token: 'STRIPE_TOKEN',
   paypal_token: 'AUqwOlb75yY_oKcU0hud9J8Rjc0_dVJJn5qHSeYZJ-eIBk-iQl5dI7o_isPd5l68aP1WjZkZKdvay1_a',
   // baseUrl: 'http://localhost:5000/api/v1/',
-  baseUrl: 'http://13.217.198.39:5000/api/v1/',
+  baseUrl: 'https://api.ralbatech.com/api/v1/',
   storeUrl: 'https://store.ralbatech.com'
 };
 
